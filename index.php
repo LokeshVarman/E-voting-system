d0edc15002a577f0baba7225b9053c79f5ba369d
lokesh varman <lokivarman5@gmail.com>
2018-02-09 07:28:19 +0530

    Add existong file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include "header.php";?>
+<div id="cover">
+<div id="content">
+<?php global $msg; echo $msg;?>
+<br/><h3>HEYYY!! this is an e-voting system for selecting "FAVORITE LEADER AND SCIENTIST"</h3><br/>
+<p>This system allows all registered student to vote for their FAVORITE leader and scientists </p>
+<p>In order to make a vote you have to be registered on thesystem.<br/>
+   <br/>
+    -Most recent information will be updated in this site regarding election</p>
+	
+	 
+    <p>&nbsp;&nbsp;</p>
+</div>
+</div>
+<?php include "footer.php";?>
